1196640f743d452080770c0748a43d93e9b62653
Baek-seungeun <sdf3872@gmail.com>
2023-11-24 00:18:32 +0900

    Fragment

--- app/src/main/java/com/example/market/MainActivity.kt
+++ app/src/main/java/com/example/market/MainActivity.kt
@@ -1,38 +1,56 @@
 package com.example.market
 
-
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.view.View
 import androidx.activity.OnBackPressedCallback
+import androidx.appcompat.app.AppCompatActivity
+import com.example.market.databinding.ActivityMainBinding
 
-
+@Suppress("DEPRECATION")
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         Handler(Looper.getMainLooper()).postDelayed({
-            val intent = Intent(this, HomeActivity::class.java)
-            intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
-            startActivity(intent)
-            finish()
-        },DURATION)
+            // After 3 seconds, replace with ListFragment
+            replaceWithListFragment()
+        }, DURATION)
 
+        // Set up onBackPressedCallback
         this.onBackPressedDispatcher.addCallback(this, callback)
-
     }
+
+    private fun replaceWithListFragment() {
+        val homeFragment = HomeFragment()
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, homeFragment)
+            .commit()
+
+        binding.fragmentContainer.visibility = View.VISIBLE
+    }
+
     companion object {
-        private const val DURATION : Long = 3000
+        private const val DURATION: Long = 3000
     }
-
 
     private val callback = object : OnBackPressedCallback(true) {
         override fun handleOnBackPressed() {
-
+            // Check if any fragment in the back stack
+            if (supportFragmentManager.backStackEntryCount > 0) {
+                // Pop the fragment from the back stack
+                supportFragmentManager.popBackStack()
+            } else {
+                // If no fragment in the back stack, handle back press as needed
+                isEnabled = false
+                onBackPressed()
+            }
         }
     }
 }
--- app/src/main/java/com/example/market/WriteFragment.kt
+++ app/src/main/java/com/example/market/WriteFragment.kt
@@ -1,41 +1,51 @@
 package com.example.market
+import android.app.Activity
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
-import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import com.google.firebase.firestore.CollectionReference
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.StorageReference
-import com.google.firebase.storage.UploadTask
 import android.webkit.MimeTypeMap
-import com.example.market.R
 
-@Suppress("DEPRECATION")
-class WriteActivity : AppCompatActivity() {
+class WriteFragment : Fragment() {
 
     private val PICK_IMAGE_REQUEST = 1
     private var imageUri: Uri? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_write)
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_write, container, false)
+    }
 
-        val saveImageButton: Button = findViewById(R.id.addImageButton)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val saveImageButton: Button = view.findViewById(R.id.addImageButton)
         saveImageButton.setOnClickListener {
             openFileChooser()
         }
-        val saveButton = findViewById<Button>(R.id.button)
+
+        val saveButton = view.findViewById<Button>(R.id.button)
         saveButton.setOnClickListener {
             saveDataToFirestore()
-            Toast.makeText(this@WriteActivity, "글쓰기 성공", Toast.LENGTH_SHORT).show()
-            val intent = Intent(this, ListActivity::class.java)
-            startActivity(intent)
+            Toast.makeText(requireContext(), "글쓰기 성공", Toast.LENGTH_SHORT).show()
 
+            // ListFragment로 전환
+            val transaction = requireActivity().supportFragmentManager.beginTransaction()
+            transaction.replace(R.id.fragment_container, ListFragment())
+            transaction.addToBackStack(null)
+            transaction.commit()
         }
     }
 
@@ -48,26 +58,26 @@ class WriteActivity : AppCompatActivity() {
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
-        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.data != null) {
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == Activity.RESULT_OK && data != null && data.data != null) {
             imageUri = data.data
             saveDataToFirestore()
         }
     }
 
     private fun saveDataToFirestore() {
-        val title = findViewById<EditText>(R.id.titleContent)
-        val price = findViewById<EditText>(R.id.priceContent)
-        val content = findViewById<EditText>(R.id.contentView)
-        val name = findViewById<EditText>(R.id.nameContent)
+        val title = view?.findViewById<EditText>(R.id.titleContent)
+        val price = view?.findViewById<EditText>(R.id.priceContent)
+        val content = view?.findViewById<EditText>(R.id.contentView)
+        val name = view?.findViewById<EditText>(R.id.nameContent)
 
         val db = FirebaseFirestore.getInstance()
         val itemsCollection: CollectionReference = db.collection("products")
 
-        val itemData = hashMapOf<String, Any>(
-            "title" to title.text.toString(),
-            "price" to price.text.toString(),
-            "content" to content.text.toString(),
-            "name" to name.text.toString(),
+        val itemData = hashMapOf(
+            "title" to title?.text.toString(),
+            "price" to price?.text.toString(),
+            "content" to content?.text.toString(),
+            "name" to name?.text.toString(),
             "sell" to "판매중" // Default status
         )
 
@@ -78,7 +88,7 @@ class WriteActivity : AppCompatActivity() {
         }
     }
 
-    private fun uploadImageAndSaveData(itemData: HashMap<String, Any>, itemsCollection: CollectionReference) {
+    private fun uploadImageAndSaveData(itemData: HashMap<String, String>, itemsCollection: CollectionReference) {
         val storageRef: StorageReference = FirebaseStorage.getInstance().getReference("item_images")
         val imageName = "${System.currentTimeMillis()}.${getFileExtension(imageUri!!)}"
 
@@ -91,23 +101,22 @@ class WriteActivity : AppCompatActivity() {
                     }
             }
             .addOnFailureListener { e ->
-                Toast.makeText(this, "Error uploading image", Toast.LENGTH_SHORT).show()
+                Toast.makeText(requireContext(), "Error uploading image", Toast.LENGTH_SHORT).show()
             }
     }
 
-    private fun saveDataWithoutImage(itemData: HashMap<String, Any>, itemsCollection: CollectionReference) {
+    private fun saveDataWithoutImage(itemData: HashMap<String, String>, itemsCollection: CollectionReference) {
         itemsCollection.add(itemData)
             .addOnSuccessListener { documentReference ->
-                Toast.makeText(this, "Data saved to Firestore", Toast.LENGTH_SHORT).show()
-
+                Toast.makeText(requireContext(), "Data saved to Firestore", Toast.LENGTH_SHORT).show()
             }
             .addOnFailureListener { e ->
-                Toast.makeText(this, "Error saving data to Firestore", Toast.LENGTH_SHORT).show()
+                Toast.makeText(requireContext(), "Error saving data to Firestore", Toast.LENGTH_SHORT).show()
             }
     }
 
     private fun getFileExtension(uri: Uri): String? {
-        val contentResolver = contentResolver
+        val contentResolver = requireActivity().contentResolver
         val mimeTypeMap = MimeTypeMap.getSingleton()
         return mimeTypeMap.getExtensionFromMimeType(contentResolver.getType(uri))
     }
